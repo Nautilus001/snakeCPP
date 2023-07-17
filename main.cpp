47c2dd4886d42224e82c86e8dbf2f8fde93a7f9f
Justin <jbush023@uottawa.ca>
2023-07-17 19:48:39 -0400

    created snake constructor, began work on methods

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-const int BOARD_WIDTH = 50;
-const int BOARD_HEIGHT = 50;
+const int BOARD_WIDTH = 16;
+const int BOARD_HEIGHT = 16;
+
 char gameBoard[BOARD_HEIGHT][BOARD_WIDTH];
+int snakePosition[256][256];
+
+class Snake{
+public:
+    enum class Direction {UP, DOWN, LEFT, RIGHT};
+
+    Direction direction;
+    int size;
+
+    Snake(int startX, int startY, int initialSize) : direction(Direction::RIGHT), size(initialSize){
+        /* for(i = 0; i < size - 1; i++){
+            body.grow(std::make_pair(startX - i, startY));
+        }
+        */
+    }
+}
 
 void initializeGameBoard(){
     for (int i = 0; i < BOARD_HEIGHT; i++){
@@ -26,7 +44,12 @@ void renderGameBoard(){
         cout << endl;
     }
 }
+void updateSnakePos(){
+
+}
 
 int main(){
+    initializeGameBoard();
+    renderGameBoard();
     return 0;
 }
